cd753a6f75254f61858a8a14a7bc93cccddf22f9
1 <martin@pixelpudu.com>
2017-11-02 10:19:24 +0000

    updates
    Left content on legal pages etc

--- page-cookies.php
+++ page-cookies.php
@@ -20,7 +20,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'center' );
+				get_template_part( 'template-parts/content', 'left' );
 
 			endwhile; // End of the loop.
 			?>
---
+++ template-parts/content-left.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Daneh
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="content left">
+
+		<header>
+			<h1><?php the_title(); ?></h1>
+		</header>
+
+		<?php the_content(); ?>
+	</div>
+
+</article>
