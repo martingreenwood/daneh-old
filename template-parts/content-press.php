ef46ae748bae2d06c86f2de572e5f606b36ed4cf
1 <martin@pixelpudu.com>
2017-11-09 16:29:49 +0000

    fix

--- template-parts/content-press.php
+++ template-parts/content-press.php
@@ -39,7 +39,11 @@
 		<div class="glossy">
 			<?php $pressfile = get_field( 'press_pdf' ); ?>
 			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail( 'full' ); ?>
+				<?php has_post_thumbnail(  ): ?>
+					<?php the_post_thumbnail( 'full' ); ?>
+				<?php else: ?>
+					<img src="http://www.danehdesign.com/wp-content/uploads/2017/11/daneh.jpg" alt="">
+				<?php endif; ?>
 				<h3><?php the_title( ); ?></h3>
 				<h6><?php echo get_the_date( ); ?></h6>
 			</a>
