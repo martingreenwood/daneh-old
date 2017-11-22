71906bb96fadb458f08f6ea22bce157f37add453
1 <martin@pixelpudu.com>
2017-11-22 09:16:14 +0000

    Fix mobile image

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -12,16 +12,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php 
-	
 	if (get_field( 'images_video' )):
 	$home_page_box_image = get_field( 'home_page_box_image' );
 	$home_page_tall_image = get_field( 'home_page_tall_image' );
-	
 	?>
-	<div id="home_page_box_image" class="coverimage <?php if(!$home_page_tall_image): ?>wide<?php endif; ?>" style="background-image: url(<?php echo $home_page_box_image; ?>)"></div>
+	<div id="home_page_box_image" class="coverimage <?php if (get_field( 'mobile_image_overide' )):?>hideonmobile<?php endif; ?>" <?php if(!$home_page_tall_image): ?>wide<?php endif; ?>" style="background-image: url(<?php echo $home_page_box_image; ?>)"></div>
+
+	<?php if (get_field( 'mobile_image_overide' )): ?>
+	<div id="home_page_box_image_mobile" class="coverimage" style="background-image: url(<?php echo get_field( 'mobile_image' );; ?>)"></div>
+	<?php endif ?>
 
 	<?php if ($home_page_tall_image): ?>
-	<div id="home_page_tall_image" class="coverimage" style="background-image: url(<?php echo $home_page_tall_image; ?>)"></div>
+	<div id="home_page_tall_image" class="coverimage <?php if (get_field( 'mobile_image_overide' )):?>hideonmobile<?php endif; ?>" style="background-image: url(<?php echo $home_page_tall_image; ?>)"></div>
 	<?php endif ?>
 
 	<?php else: ?>
