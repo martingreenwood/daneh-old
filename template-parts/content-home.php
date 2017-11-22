4915cdd805aa7c3e5396b4c0e9c3d9457148cac4
1 <martin@pixelpudu.com>
2017-11-22 09:24:38 +0000

    fix

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -16,10 +16,10 @@
 	$home_page_box_image = get_field( 'home_page_box_image' );
 	$home_page_tall_image = get_field( 'home_page_tall_image' );
 	?>
-	<div id="home_page_box_image" class="coverimage <?php if (get_field( 'mobile_image_overide' )):?>hideonmobile<?php endif; ?>" <?php if(!$home_page_tall_image): ?>wide<?php endif; ?>" style="background-image: url(<?php echo $home_page_box_image; ?>)"></div>
+	<div id="home_page_box_image" class="coverimage <?php if (get_field( 'mobile_image_overide' )): ?>hideonmobile <?php endif; ?> <?php if(!$home_page_tall_image): ?>wide<?php endif; ?>" style="background-image: url(<?php echo $home_page_box_image; ?>)"></div>
 
 	<?php if (get_field( 'mobile_image_overide' )): ?>
-	<div id="home_page_box_image_mobile" class="coverimage" style="background-image: url(<?php echo get_field( 'mobile_image' );; ?>)"></div>
+	<div id="home_page_box_image_mobile" class="coverimage" style="background-image: url(<?php echo get_field( 'mobile_image' ); ?>)"></div>
 	<?php endif ?>
 
 	<?php if ($home_page_tall_image): ?>
